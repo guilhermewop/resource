795a9dc8093f31408415d52ca9157f868eaba7f4
Guilherme W. O. Pereira <guilherme.pereira@predicta.net>
2015-01-07 21:07:05 -0200

    teste

--- test.php
+++ test.php
@@ -7,7 +7,7 @@ $loader = require __DIR__.'/vendor/autoload.php';
 
 use Level3\Resource\Format\Reader\HAL;
 
-$json = file_get_contents('/tmp/json-tests/newspaper-item.json');
+$json = file_get_contents('newspaper-item.json');
 
 $reader = new HAL\JsonReader();
 $resource = $reader->execute($json);
